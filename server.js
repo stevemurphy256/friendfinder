8e0b7c8722543443467fd11541ae2e160c64b962
stevemurphy256 <smurphy5884@icloud.com>
2017-09-21 09:19:27 -0400

    npm init done

---
+++ server.js
@@ -0,0 +1,4 @@
+// Your `server.js` file should require the basic npm packages we've used in class: `express`, `body-parser` and `path`.
+var express = require('express');
+var body-parser = require('body-parser');
+var path = require('path');

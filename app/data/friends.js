e5c8adabdb4506861eb59f582ea1b0d13b9c6c21
stevemurphy256 <smurphy5884@icloud.com>
2017-09-22 18:30:32 -0400

    server configured and tested html started

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,168 @@
-// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+var friends = [ 
+	{
+	"name": "Mickey Mouse",
+	"photo": "https://www.disneyclips.com/imagesnewb/images/mickey-mouse3.png",
+	"scores": [
+	  "5",
+	  "1",
+	  "4",
+	  "4",
+	  "5",
+	  "1",
+	  "2",
+	  "5",
+	  "4",
+	  "1"
+	]
+	},
+	{
+	"name": "Minnie Mouse",
+	"photo": "https://www.disneyclips.com/imagesnewb/images/classic-minnie6.png",
+	"scores": [
+	  "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+	]
+	},
+	{
+	"name": "Donald Duck",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/618px-Donald_Duck.svg.png",
+	"scores": [ 
+	  "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+	]
+	},
+	{
+	"name": "Goofy",
+	"photo": "https://www.disneyclips.com/imagesnewb/images/goofy-face2.png",
+	"scores": [
+	  "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+	]
+	},
+	{
+	"name": "Pluto",
+	"photo": "https://www.disneyclips.com/imagesnewb/images/pluto11.png",
+	"scores": [
+	  "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+	]
+	},
+	{
+	"name": "Cinderella",
+	"photo": "https://www.disneyclips.com/imagesnewb3/images/cinderella-flowers.png",
+	"scores": [
+	  "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+	]
+	},
+	{
+	"name": "Snow White",
+	"photo": "https://www.disneyclips.com/imagesnewb4/snowwhite.png",
+	"scores": [
+	  "1",
+	  "3",
+	  "2",
+	  "3",
+	  "4",
+	  "3",
+	  "2",
+	  "4",
+	  "5",
+	  "1"
+	]
+	},
+	{
+	"name": "Pocahontas",
+	"photo": "https://www.disneyclips.com/imagesnewb/images/pocahontas6.png",
+	"scores": [
+	"3",
+	"3",
+	"4",
+	"4",
+	"3",
+	"3",
+	"5",
+	"1",
+	"1",
+	"4"
+	]
+	},
+	{
+	"name": "Grumpy",
+	"photo": "https://www.disneyclips.com/imagesnewb4/images/grumpy2.png",
+	"scores": [
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"4",
+	"1",
+	"1",
+	"2"
+	]
+	},
+	{
+	"name": "Sleepy",
+	"photo": "https://www.disneyclips.com/imagesnewb4/images/sleepy.png",
+	"scores": [
+	"2",
+	"2",
+	"5",
+	"5",
+	"5",
+	"1",
+	"1",
+	"1",
+	"2",
+	"2"
+	]
+}
+];
 
+module.exports = friends;
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 // ```json
 // {
 //   "name":"Ahmed",
--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 // Your `server.js` file should require the basic npm packages we've used in class: `express`, `body-parser` and `path`.
 var express = require('express');
-var body-parser = require('body-parser');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+// sets up express server
+var app = express();
+
+// sets the PORT
+var PORT = process.env.PORT || 3000;
+
+// sets up express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api/json "}));
+
+// router files required 
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing.htmlRoutes")(app);
+
+// start the server
+app.listen(PORT, function() {
+	console.log("App listening on port: " + PORT);
+});

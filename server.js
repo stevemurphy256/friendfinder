63401e0b361cfe5eeb380d24186e279d7d72c53e
stevemurphy256 <smurphy5884@icloud.com>
2017-09-24 18:29:47 -0400

    periodic save

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api/json "}));
 
 // router files required 
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // start the server
 app.listen(PORT, function() {

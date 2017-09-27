ab275e08affe487e2c52348632a6ff49bd2f9919
stevemurphy256 <smurphy5884@icloud.com>
2017-09-27 10:55:23 -0400

    fixed some typos

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,6 @@
-// Your `apiRoutes.js` file should contain two routes:
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
 // load data
-var friendList = require('../data/friends.js');
+var friendList = require('../data/friend.js');
 
 module.exports = function(app) {
 	// a GET route that displays a JSON of all friends
@@ -15,19 +13,36 @@ module.exports = function(app) {
 		var newSurveyScores = req.body.scores;
 		var scoresArray = [];
 		var friendCount = 0;
-		var bestMatch = 0;
+		var closestMatch = 0;
 
 		// loop through friendList array
 		for (var i = 0; i < friendList.length; i++) {
 			var scoresDiff = 0;
 			// loop through scores to compare friends
 			for (var j = 0; j < newSurveyScores.length; j++) {
-				scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newSurveyScores[j])));
+				scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newSurveyScores[j])));			
+			}
 
-				// push results into scoresArray
+			// push results into scoresArray
 				scoresArray.push(scoresDiff);
 			}
-		}
-	})
 
-}
+			// after friends are compared find the closest match
+			for (var i = 0; i<scoresArray.length;i++) {
+				if(scoresArray[i] <= scoresArray[closestMatch]){
+					closestMatch = i;
+				}
+			}
+
+			// return closest match data
+			var disneyFriend = friendList[closestMatch];
+			res.json(disneyFriend);
+
+			// push new survey submission into friendList array
+			friendList.push(req.body);
+		});
+	};
+// Your `apiRoutes.js` file should contain two routes:
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
+//      This route will also be used to handle the compatibility logic. 

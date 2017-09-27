429a5417a3a62968f63149dee40acf660741602c
stevemurphy256 <smurphy5884@icloud.com>
2017-09-27 14:17:50 -0400

    update filename

---
+++ app/data/friend.js
@@ -0,0 +1,151 @@
+var friendList = [ 
+	{
+	"name": "Mickey Mouse",
+	"profilePic": "https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png",
+	"scores": [
+	  "5",
+	  "1",
+	  "4",
+	  "4",
+	  "5",
+	  "1",
+	  "2",
+	  "5",
+	  "4",
+	  "1"
+	]
+	},
+	{
+	"name": "Minnie Mouse",
+	"profilePic": "http://images.shirts.com/products/19853/13-14/infant-minnie-mouse-face-t-shirt.jpg",
+	"scores": [
+	  "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+	]
+	},
+	{
+	"name": "Donald Duck",
+	"profilePic": "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/618px-Donald_Duck.svg.png",
+	"scores": [ 
+	  "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+	]
+	},
+	{
+	"name": "Goofy",
+	"profilePic": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Goofy.svg/690px-Goofy.svg.png",
+	"scores": [
+	  "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+	]
+	},
+	{
+	"name": "Pluto",
+	"profilePic": "http://www.dodogs.website/wp-content/uploads/2015/04/Pluto.jpg",
+	"scores": [
+	  "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+	]
+	},
+	{
+	"name": "Cinderella",
+	"profilePic": "http://images.shirts.com/products/27423/14-14-58843/cinderella-castle-silo-juniors-tank-image2.jpg",
+	"scores": [
+	  "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+	]
+	},
+	{
+	"name": "Snow White",
+	"profilePic": "https://upload.wikimedia.org/wikipedia/en/e/e1/Snow_white_disney.png",
+	"scores": [
+	  "1",
+	  "3",
+	  "2",
+	  "3",
+	  "4",
+	  "3",
+	  "2",
+	  "4",
+	  "5",
+	  "1"
+	]
+	},
+	{
+	"name": "Peter Pan",
+	"profilePic": "https://vignette3.wikia.nocookie.net/disney/images/f/f8/Peter_Pan_Transparent.png/revision/latest?cb=20151220232312",
+	"scores": [
+	"3",
+	"3",
+	"4",
+	"4",
+	"3",
+	"3",
+	"5",
+	"1",
+	"1",
+	"4"
+	]
+	}
+];
+
+module.exports = friendList;
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+// ```json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+// ```
